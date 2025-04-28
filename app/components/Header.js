a0fb9e34b27e67d941b4ad91e651fa49ad0d5c8d
alexander784 <alexandernyaga1999@gmail.com>
2025-04-28 14:13:34 +0300

    Change folder structure

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,41 +1,42 @@
-import React from 'react';
 import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="text-white">
+    <header className="text-white bg-gray-800 sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 py-6 flex justify-between items-center">
         <h1 className="text-3xl font-bold">Sparkle Cleaning Co.</h1>
-        <nav>
-          <ul className="flex space-x-6">
-            <li>
+        <nav className="flex items-center">
+          <ul className="flex items-center space-x-8">
+            <div className="flex space-x-10">
+              <li>
+                <Link
+                  href="/"
+                  className="text-white cursor-pointer hover:text-blue-400 transition duration-200"
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/services"
+                  className="text-white cursor-pointer hover:text-blue-400 transition duration-200"
+                >
+                  Services
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/gallery"
+                  className="text-white cursor-pointer hover:text-blue-400 transition duration-200"
+                >
+                  Gallery
+                </Link>
+              </li>
+            </div>
+            <li className="ml-10">
               <Link
-                href="/"
-                className="text-white cursor-pointer hover:underline transition duration-200"
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="#services"
-                className="text-white cursor-pointer hover:underline transition duration-200"
-              >
-                Services
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="#gallery"
-                className="text-white cursor-pointer hover:underline transition duration-200"
-              >
-                Gallery
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/Info"
-                className="text-white cursor-pointer hover:underline transition duration-200"
+                href="/info"
+                className="text-white cursor-pointer hover:text-orange-400 transition duration-200"
               >
                 Contact
               </Link>
--- app/page.js
+++ app/page.js
@@ -1,9 +1,8 @@
-import Header from "./components/Header";
 import Hero from "./components/Hero";
-import Services from "./components/Services";
-import Gallery from "./components/Gallery";
+import Services from "./services/page";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import Gallery from "./Gallery/page";
 
 export default function Home() {
   return (
@@ -12,7 +11,6 @@ export default function Home() {
         className="relative bg-cover bg-center h-screen"
         style={{ backgroundImage: "url('/images/Cleaning.jpg')" }}
       >
-        <Header />
         <Hero />
       </div>
       <Services />
